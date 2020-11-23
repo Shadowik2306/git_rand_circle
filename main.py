b39f6a0154c07f863144983a12a6b6ab6cb6541a
shadowik <zahar.mark@bk.ru>
2020-11-24 00:41:35 +0400

    Random color is cool

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
 from random import randint
+from UI import Ui_Form
 
 SCREEN_SIZE = (696, 501)
 
 
-class Window(QWidget):
+class Window(QWidget, Ui_Form):
     def __init__(self):
         super(Window, self).__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.drawing = False
         self.pushButton.clicked.connect(self.draw_start)
 
@@ -18,13 +18,14 @@ class Window(QWidget):
         self.x = randint(0, SCREEN_SIZE[0])
         self.y = randint(0, SCREEN_SIZE[1])
         self.d = randint(10, 100)
+        self.r, self.g, self.b = randint(0, 255), randint(0, 255), randint(0, 255)
         self.drawing = True
 
     def paintEvent(self, event):
         if self.drawing:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 211, 0))
+            qp.setBrush(QColor(self.r, self.g, self.b))
             qp.drawEllipse(self.x, self.y, self.d, self.d)
             qp.end()
             self.update()
